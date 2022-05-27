c699b74726a1d8a2a927ec35d1fc2c276ef1ea2e
wusong <wusong3918@gmail.com>
2022-05-27 20:23:41 +0800

    2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const selectionindex = ref(null)
 const DATAmap = {}
 
 function clickbutton(i, color) {
-
+  i = "$" + i + "$"
   DATAmap[i] = color
   let text = dom.value.innerText
   if (selectionindex) {
@@ -20,28 +20,56 @@ function clickbutton(i, color) {
   }
 }
 
-function blur1() {
+function calculaIndex(e) {
   selectionindex.value = getSelection()?.anchorOffset
+}
+
+function doInput(e) {
+  vmtext.value = e.target.innerText
+  calculaIndex()
 }
 
 function output(text) {
   for (const datAmapKey in DATAmap) {
     text = text.replaceAll(datAmapKey, `<span style="color: ${DATAmap[datAmapKey]}">${datAmapKey}</span>`)
   }
-  console.log(text)
+
   return text
 }
 
+function deleteOne(e) {
+  let allString = vmtext.value
+  const lastString = allString.slice(selectionindex.value - 1, selectionindex.value)
+  console.log(lastString)
+  if (lastString === "$") {
+    e.preventDefault()
+    let list = Array.from(allString.slice(0, selectionindex.value - 1))
+    console.log(list)
+    let i = 1
+    while (list.length) {
+      i++
+      if (list.pop() === "$") {
+        const start = selectionindex.value - i
+        const end = selectionindex.value
+        vmtext.value = allString.slice(0, start) + allString.slice(end)
+        dom.value.innerText=vmtext.value
+      }
+    }
+
+  }
+
+}
 </script>
 
 <template>
   光标{{ selectionindex }}
-  <button @click="clickbutton('vue','red')" style="color: red">vue</button>
-  <button @click="clickbutton('js','green')" style="color: green">js</button>
-  <button @click="clickbutton('css','blue')" style="color:blue">css</button>
+  <button @click="clickbutton('创建人姓名','red')" style="color: red">创建人姓名</button>
+  <button @click="clickbutton('创建人id','green')" style="color: green">创建人id</button>
+
 
   <div>
-    <div @blur="blur1" @input="e=>vmtext=e.target.innerText" @keydown.enter="e=>e.preventDefault()" ref="dom"
+    <div @keydown.delete="deleteOne" @click="calculaIndex" @keydown="calculaIndex" @input="doInput"
+         @keydown.enter="e=>e.preventDefault()" ref="dom"
          contenteditable="true"
          style="width: 300px;height:100px;border: 1px black solid;position: absolute;color: rgba(0,0,0,0)"></div>
     <div v-html="output(vmtext)"
